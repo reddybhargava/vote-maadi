a947943847e8a0482076387aa9232f8a2913d23a
Bhargava Reddy <bharghava.n.reddy@gmail.com>
2020-03-31 17:23:22 +0530

    added election route

--- server.js
+++ server.js
@@ -1,16 +1,23 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const cloudinary = require('cloudinary').v2;
+const config = require('config');
+const cloudinaryConfig = config.get('cloudinaryConfig');
 
 const app = express();
 
 // Connect database
 connectDB();
 
+// Configure cloudinary (for uploading images)
+cloudinary.config(cloudinaryConfig);
+
 // Init Middleware
 app.use(express.json({ extended: false }));
 
 // Define routes
 app.use('/api/users', require('./routes/api/users'));
+app.use('/api/elections', require('./routes/api/elections'));
 // app.use('/api/auth', require('./routes/api/auth'));
 
 const PORT = process.env.PORT || 5000;
