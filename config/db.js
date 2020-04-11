eaf3ca4d1d587850b3e60c206e265387639355df
Bhargava Reddy <49948044+reddybhargava@users.noreply.github.com>
2020-04-11 11:28:17 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,11 +2,9 @@ const mongoose = require('mongoose');
 const config = require('config');
 const db = config.get('mongoURI');
 
-const uri = "mongodb+srv://root:123@cluster0-lawgb.mongodb.net/test?retryWrites=true&w=majority";
-
 const connectDB = async () => {
 	try {
-		await mongoose.connect(uri, {
+		await mongoose.connect(db, {
 			useNewUrlParser: true,
 			useUnifiedTopology: true,
 			useCreateIndex: true
