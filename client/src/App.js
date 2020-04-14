203da65094eeb3ba08ec140e37f00e103d0b5fd2
aishwarya <aishwaryakalgudi@gmail.com>
2020-04-14 10:19:56 +0530

    nav bar fixed

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,22 @@
 import React, { Fragment, Component } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch, Link} from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 import SignIn from './components/auth/SignIn';
 import SignUp from './components/auth/SignUp';
-import Election from './components/pages/Election';
+import AddElection from './components/pages/AddElection';
 import AddCandidate from './components/pages/AddCandidate';
 import './App.css';
+import OngoingElections from './components/pages/OngoingElections';
+import Election from './components/pages/Election';
+import {v4 as uuid} from 'uuid';
 
 class App extends Component {
 
 	state = {
 		loggedIn: false,
 		name: '',
+		// type: '',
 		token: ''
 	}
 
@@ -27,17 +31,20 @@ class App extends Component {
 		this.setState({token: token})
 	}
 
-	render() {		
+	render() {
+			
 		return (
 		<Router>
 			<Fragment>
-				<Navbar />
+				<Navbar app={this.state} />
 				<Route exact path="/" component={Landing} />
 				<section className="container">
 					<Switch>
+						<Route exact path="/view/ongoing" component={ () => <OngoingElections app={this.state} />} />
+						<Route exact path="/view/ongoing/election" component={Election} />
 						<Route exact path="/accounts/signup" component={ () => <SignUp logIn={this.logIn} setName={this.setName} setToken={this.setToken} app={this.state}/>} />
 						<Route exact path="/accounts/signin" component={ () => <SignIn logIn={this.logIn} setName={this.setName} setToken={this.setToken} app={this.state}/>} />
-						<Route exact path="/admin/elections" component={ () => <Election app={this.state} />} />
+						<Route exact path="/admin/elections" component={ () => <AddElection app={this.state} />} />
 						<Route exact path="/admin/candidates" component={AddCandidate} />
 					</Switch>
 				</section>
--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,27 +1,34 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, {Fragment} from 'react';
+import { Link } from "react-router-dom";
 
-const Navbar = () => {
-	return (
-		<nav className="navbar navbar-dark bg-transparent">
-			<h1>
-				<Link to="/">
-					<i className="fas fa-code"></i> Vote Maadi
-				</Link>
-			</h1>
-			<ul>
-				<li>
-					<Link to="/admin/elections">Host Election</Link>
-				</li>
-				<li>
-					<Link to="/accounts/signup">Sign Up</Link>
-				</li>
-				<li>
-					<Link to="/accounts/signin">Sign In</Link>
-				</li>
-			</ul>
-		</nav>
-	);
+const Navbar = (props) => {
+  const items =
+    props.app.loggedIn === false ? (
+      <Fragment>
+        <li>
+          <Link to="/accounts/signup">Sign Up</Link>
+        </li>
+        <li>
+          <Link to="/accounts/signin">Sign In</Link>
+        </li>
+      </Fragment>
+    ) : (
+      <li>
+        <Link to="/admin/elections">Host Election</Link>
+      </li>
+    );
+  return (
+    <nav className="navbar navbar-dark bg-transparent">
+      <h1>
+        <Link to="/">
+          <i className="fas fa-code"></i> Vote Maadi
+        </Link>
+      </h1>
+      <ul>
+        {items}
+      </ul>
+    </nav>
+  );
 };
 
 export default Navbar;
