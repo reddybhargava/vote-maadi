8d10b1064ed14a114e6b46c80ebfe0ebc66bd838
Bhargava Reddy <bharghava.n.reddy@gmail.com>
2020-04-15 13:53:45 +0530

    added uploading image for candidates

--- client/src/components/pages/AddCandidate.js
+++ client/src/components/pages/AddCandidate.js
@@ -4,16 +4,16 @@ import axios from 'axios';
 
 export class AddCandidate extends Component {
 	state = {
-        name: '',
-        promises: '',
-        gender: '',
+		name: '',
+		promises: '',
+		gender: '',
 		age: '',
 		selectedFile: null,
 		csv: null,
-		redirect: false,
+		redirect: false
 	};
 
-	onChange = (e) => this.setState({ [e.target.name] : e.target.value });
+	onChange = (e) => this.setState({ [e.target.name]: e.target.value });
 
 	onFileChange = (event) => {
 		// Update the state
@@ -25,51 +25,63 @@ export class AddCandidate extends Component {
 		this.setState({ csv: event.target.files[0] });
 	};
 
-  	addCandidate = async (e) => {
+	addCandidate = async (e) => {
 		e.preventDefault();
 		console.log(this.props.location.state.token);
-		const newCandidate = {
-		  name: this.state.name,
-		  promises: this.state.promises,
-		  gender: this.state.gender,
-		  age: this.state.age,
-		  selectedFile : this.state.selectedFile,
-		  csv : this.state.csv
+		// const newCandidate = {
+		//   name: this.state.name,
+		//   promises: this.state.promises,
+		//   gender: this.state.gender,
+		//   age: this.state.age,
+		//   selectedFile : this.state.selectedFile,
+		//   csv : this.state.csv
+		// }
+
+		let formData = new FormData();
+		if (this.state.selectedFile !== null) {
+			formData.append(
+				'image',
+				this.state.selectedFile,
+				this.state.selectedFile.name
+			);
 		}
+		formData.append('name', this.state.name);
+		formData.append('promises', this.state.promises);
+		formData.append('gender', this.state.gender);
+		formData.append('age', this.state.age);
+		console.log(formData);
+
 		const config = {
 			headers: {
-			  "Content-Type": "application/json",
-			  "x-auth-token": this.props.location.state.token
+				'x-auth-token': this.props.location.state.token
 			}
-		  };
+		};
 
-		const url = "http://localhost:3000/api/elections/" +this.props.location.state.electionId + "/candidates";
+		const url =
+			'http://localhost:3000/api/elections/' +
+			this.props.location.state.electionId +
+			'/candidates';
 		console.log(url);
 
 		try {
-			const res = await axios.post(
-			  url,
-			  [newCandidate],
-			  config
-			);
+			const res = await axios.post(url, formData, config);
 			console.log(res);
-		  } catch (error) {
+		} catch (error) {
 			const response = error.response;
 			console.log(response.data);
 		}
-	}	
+	};
 
-    
-    onFinish =  (e) => {
+	onFinish = (e) => {
 		this.addCandidate(e);
-		this.setState({redirect : true});
+		this.setState({ redirect: true });
 	};
 
 	render() {
 		if (this.state.redirect === true) {
 			return <Redirect to="/" />;
 		}
-		return (			
+		return (
 			<Fragment>
 				<h1 className="large text-primary">Candidate Details</h1>
 				<p className="lead">Fill in the candidate details</p>
@@ -96,24 +108,24 @@ export class AddCandidate extends Component {
 						/>
 					</div>
 
-                    <div className="form-group">
-                        <input
-                            type="radio"
-                            name="gender"
-                            value="Male"
-                            onChange={this.onChange}
-                        />
-                        <span className="lead"> Male </span>
-                        <input
-                            type="radio"
-                            name="gender"
-                            value="Female"
-                            onChange={this.onChange}
-                        />
-                        <span className="lead"> Female </span>
-                    </div>
+					<div className="form-group">
+						<input
+							type="radio"
+							name="gender"
+							value="Male"
+							onChange={this.onChange}
+						/>
+						<span className="lead"> Male </span>
+						<input
+							type="radio"
+							name="gender"
+							value="Female"
+							onChange={this.onChange}
+						/>
+						<span className="lead"> Female </span>
+					</div>
 
-                    <div className="form-group">
+					<div className="form-group">
 						<input
 							type="text"
 							placeholder="promises"
@@ -124,32 +136,42 @@ export class AddCandidate extends Component {
 						/>
 					</div>
 					<div className="form-group">
-						<input 
-							type="reset" 
-							value="Add Candidate"
-						/>
+						<input type="reset" value="Add Candidate" />
 						{/* </input> */}
-					</div>					
+					</div>
 				</form>
 
 				<div className="form-group">
-					Upload Candidate image : 
-            		<input type="file" className="img" onChange={this.onFileChange} />
-          		</div>
+					Upload Candidate image :
+					<input
+						type="file"
+						className="img"
+						onChange={this.onFileChange}
+					/>
+				</div>
 
 				<div className="form-group">
-					Upload Voters list : 
-            	<input type="file" className="img" onChange={this.onCSVChange} />
-          		</div>
-
-				<button className="btn btn-primary" onClick={this.addCandidate}> Add Candidate</button>
-				<div>
-                	<button className="btn btn-primary" onClick={this.onFinish}> Save and Finish</button>
+					Upload Voters list :
+					<input
+						type="file"
+						className="img"
+						onChange={this.onCSVChange}
+					/>
 				</div>
-		</Fragment>
-		)
+
+				<button className="btn btn-primary" onClick={this.addCandidate}>
+					{' '}
+					Add Candidate
+				</button>
+				<div>
+					<button className="btn btn-primary" onClick={this.onFinish}>
+						{' '}
+						Save and Finish
+					</button>
+				</div>
+			</Fragment>
+		);
 	}
 }
 
 export default AddCandidate;
-
