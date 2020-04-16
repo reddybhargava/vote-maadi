7733ceb40c48c76859e3e68c9932eafcc89792ce
Bhargava Reddy <bharghava.n.reddy@gmail.com>
2020-04-16 17:02:35 +0530

    added route for election results page

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { Fragment, Component } from 'react';
-import { BrowserRouter as Router, Route, Switch, Link} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 import SignIn from './components/auth/SignIn';
@@ -9,48 +9,95 @@ import AddCandidate from './components/pages/AddCandidate';
 import './App.css';
 import OngoingElections from './components/pages/OngoingElections';
 import Election from './components/pages/Election';
-import {v4 as uuid} from 'uuid';
+import ElectionResults from './components/pages/ElectionResults';
+import { v4 as uuid } from 'uuid';
 
 class App extends Component {
-
 	state = {
 		loggedIn: false,
 		name: '',
 		// type: '',
 		token: ''
-	}
+	};
 
 	logIn = () => {
-		this.setState({loggedIn: true})
-	}
+		this.setState({ loggedIn: true });
+	};
 
 	setName = (user) => {
-		this.setState({name : user})
-	}
+		this.setState({ name: user });
+	};
 	setToken = (token) => {
-		this.setState({token: token})
-	}
+		this.setState({ token: token });
+	};
 
 	render() {
-			
 		return (
-		<Router>
-			<Fragment>
-				<Navbar app={this.state} />
-				<Route exact path="/" component={Landing} />
-				<section className="container">
-					<Switch>
-						<Route exact path="/view/ongoing" component={ () => <OngoingElections app={this.state} />} />
-						<Route exact path="/view/ongoing/election" component={Election} />
-						<Route exact path="/accounts/signup" component={ () => <SignUp logIn={this.logIn} setName={this.setName} setToken={this.setToken} app={this.state}/>} />
-						<Route exact path="/accounts/signin" component={ () => <SignIn logIn={this.logIn} setName={this.setName} setToken={this.setToken} app={this.state}/>} />
-						<Route exact path="/admin/elections" component={ () => <AddElection app={this.state} />} />
-						<Route exact path="/admin/candidates" component={AddCandidate} />
-					</Switch>
-				</section>
-			</Fragment>
-		</Router>
-	);}
+			<Router>
+				<Fragment>
+					<Navbar app={this.state} />
+					<Route exact path="/" component={Landing} />
+					<section className="container">
+						<Switch>
+							<Route
+								exact
+								path="/view/ongoing"
+								component={() => (
+									<OngoingElections app={this.state} />
+								)}
+							/>
+							<Route
+								exact
+								path="/view/ongoing/election"
+								component={Election}
+							/>
+							<Route
+								exact
+								path="/accounts/signup"
+								component={() => (
+									<SignUp
+										logIn={this.logIn}
+										setName={this.setName}
+										setToken={this.setToken}
+										app={this.state}
+									/>
+								)}
+							/>
+							<Route
+								exact
+								path="/accounts/signin"
+								component={() => (
+									<SignIn
+										logIn={this.logIn}
+										setName={this.setName}
+										setToken={this.setToken}
+										app={this.state}
+									/>
+								)}
+							/>
+							<Route
+								exact
+								path="/admin/elections"
+								component={() => (
+									<AddElection app={this.state} />
+								)}
+							/>
+							<Route
+								exact
+								path="/admin/candidates"
+								component={AddCandidate}
+							/>
+							<Route
+								exact
+								path="/elections/results"
+								component={ElectionResults}
+							/>
+						</Switch>
+					</section>
+				</Fragment>
+			</Router>
+		);
+	}
 }
 
 export default App;
